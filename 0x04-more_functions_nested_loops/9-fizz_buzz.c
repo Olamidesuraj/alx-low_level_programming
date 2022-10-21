bd8afa53fe4e5ccf870956e152dd260307b02a04
Olamidesuraj <olamidesuraj1@gmail.com>
2022-10-21 16:06:08 -0700

    my c more function project

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,24 +2,24 @@
 
 
 /**
- * main - print fizz_buzz
+ * main - print fizzbuzz
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	printf('1');
+	printf("1");
 	for (n = 2; n <= 100; n++)
 	{
-		printf(' ');
+		printf(" ");
 		if (n % 3 == 0)
-			printf('Fizz');
+			printf("fizz");
 		if (n % 5 == 0)
-			printf('Buzz');
+			printf("buzz");
 		if (n % 3 != 0 && n % 5 != 0)
-			printf('%d', n);
+			printf("%d", n);
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
